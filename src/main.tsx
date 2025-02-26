004b5840680ecb92a407a05dce5f342c341a389e
mbazley124 <mbazley1.mb@gmail.com>
2025-02-26 20:28:15 +0000

    Add 404.html and routing fixes2

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import routes from 'virtual:generated-pages-react';
 import Layout from './components/layout';
 import DefaultPage from './artifacts/default';
+import Projects from './artifacts/projects';
 import './index.css';
 
 const router = createBrowserRouter([
@@ -11,11 +12,16 @@ const router = createBrowserRouter([
     path: '/',
     element: <Layout><DefaultPage /></Layout>,
   },
+  {
+    path: '/projects',
+    element: <Layout><Projects /></Layout>,
+  },
   ...routes.map((route) => ({
     ...route,
     element: <Layout>{route.element}</Layout>,
   }))
 ], {
+  basename: '/',
   future: {
     v7_relativeSplatPath: true,
     v7_fetcherPersist: true,
